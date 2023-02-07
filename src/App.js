b706173010ff238e36b4fb16115e34182bf6de8f
amanpandey-03 <amanpandeyjsr307@gmail.com>
2023-02-07 06:40:26 +0530

    home created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Home from "./components/home/Home"
 
 
 function App() {
   return (
-   <Header></Header>
+  <>
+    <Header></Header>
+  
+  <main className='main'>
+    <Home/>
+   </main>
+   </>
   );
 }
 
---
+++ src/components/home/social.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Social = () => {
+   return( 
+   
+        <div className="home_social">
+            <a href="https://instagram.com/aman.on.ig" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://twitter.com/amanOnTwt" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-twitter-alt"></i>
+            </a>
+
+            <a href="https://github.com/amanpandey-03" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+   
+    )
+  
+}
+
+export default Social
